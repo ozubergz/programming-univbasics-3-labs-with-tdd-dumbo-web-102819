a794ea41fe89aca12507ed719ff997b6baa544b0
James Min <jmin499@gmail.com>
2019-10-07 02:38:09 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,4 +3,5 @@ first_number = 2
 second_number = 2
 sum = first_number + second_number
 difference = first_number - second_number
-produ
+product = first_number * second_number
+quotient = first_number / second_number
